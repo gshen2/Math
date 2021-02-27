9200f47dfaee4d08bd146358e12be4ad98a488f4
gshen2 <gshen2@uh.edu>
2021-02-27 15:12:54 -0600

    Added Circle Area and Circumference

--- Geo_2D.py
+++ Geo_2D.py
@@ -9,6 +9,15 @@ class Square:
         return self.side**2
 
 
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius**2
+
+    def circumference(self):
+        return 2 * math.pi * self.radius
 
 
-print(Square(side=3).area())
+print(Circle(radius=3).circumference())
