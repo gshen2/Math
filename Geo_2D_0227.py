be9aea22664bc0e4ea4d23f05164e58f677303ef
gshen2 <gshen2@uh.edu>
2021-03-02 07:55:26 -0600

    Trapezoid added

---
+++ Geo_2D_0227.py
@@ -0,0 +1,33 @@
+import math
+
+
+class Square:
+    def __init__(self, side):
+        self.side = side
+
+    def area(self):
+        return self.side**2
+
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius**2
+
+    def circumference(self):
+        return 2 * math.pi * self.radius
+
+
+class Trapezoid:
+    def __init__(self, top, bottom, height):
+        self.top = top
+        self.bottom = bottom
+        self.height = height
+
+    def area(self):
+        return (self.top + self.bottom)*self.height/2
+
+
+print(Circle(radius=3).circumference())
