e202cb33408dcc14fb4f8a7d28db490d70cc20f6
gshen2 <gshen2@uh.edu>
2021-03-06 14:41:58 -0600

    Updated 2D Geometries

--- Geo_2D.py
+++ Geo_2D.py
@@ -9,6 +9,51 @@ class Square:
         return self.side**2
 
 
+class Rectangle:
+    def __init__(self, base=1, height=1):
+        self.base = base
+        self.height = height
+
+    def area(self):
+        return self.base*self.height
+
+    def perimeter(self):
+        return 2*(self.base+self.height)
+
+
+class Triangle_1:
+    def __init__(self, base=1, height=1):
+        self.base = base
+        self.height = height
+
+    def area(self):
+        return 0.5*self.base*self.height
+
+
+class Triangle_2:
+    def __init__(self, side_1, side_2, side_3):
+        self.a = side_1
+        self.b = side_2
+        self.c = side_3
+
+    def perimeter(self):
+        return self.a + self.b + self.c
+
+    def area(self):
+        s = 0.5 * (self.a + self.b + self.c)
+        return math.sqrt(s*(s-self.a)*(s-self.b)*(s-self.c))
+
+
+class Trapezoid:
+    def __init__(self, top, bottom, height):
+        self.a = top
+        self.b = bottom
+        self.h = height
+
+    def area(self):
+        return 0.5 * (self.a + self.b) * self.h
+
+
 class Circle:
     def __init__(self, radius):
         self.radius = radius
@@ -18,6 +63,3 @@ class Circle:
 
     def circumference(self):
         return 2 * math.pi * self.radius
-
-
-print(Circle(radius=3).circumference())
