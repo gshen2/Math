5e856b1fc5833c7a4fd58b6a448d4c9c45feeab0
gshen2 <gshen2@uh.edu>
2021-04-03 13:27:34 -0500

    Triangle_SSS added

--- Geo_GUI_0312.py
+++ Geo_GUI_0312.py
@@ -25,9 +25,11 @@ class Window(tk.Tk):
         self.square = ttk.Frame(tabControl)
         self.rectangle = ttk.Frame(tabControl)
         self.circle = ttk.Frame(tabControl)
+        self.triangleSSS = ttk.Frame(tabControl)
         tabControl.add(self.square, text='Square')
         tabControl.add(self.rectangle, text='Rectangle')
         tabControl.add(self.circle, text='Circle')
+        tabControl.add(self.triangleSSS, text='Triangle_SSS')
         tabControl.pack(expand=1, fill='both')
         self.shapes_2d()
 
@@ -46,6 +48,45 @@ class Window(tk.Tk):
         self.sq()
         self.rect()
         self.circ()
+        self.tri_SSS()
+
+    def tri_SSS(self):
+        lbl_tri_s1 = ttk.Label(self.triangleSSS, text='Side 1 = ')
+        lbl_tri_s1.grid(row=0, column=0)
+        self.tri_s1 = tk.StringVar()
+        entry_tri_s1 = ttk.Entry(self.triangleSSS, width=10, textvariable=self.tri_s1)
+        entry_tri_s1.grid(row=0, column=1)
+
+        lbl_tri_s2 = ttk.Label(self.triangleSSS, text='Side 2 = ')
+        lbl_tri_s2.grid(row=1, column=0)
+        self.tri_s2 = tk.StringVar()
+        entry_tri_s2 = ttk.Entry(self.triangleSSS, width=10, textvariable=self.tri_s2)
+        entry_tri_s2.grid(row=1, column=1)
+
+        lbl_tri_s3 = ttk.Label(self.triangleSSS, text='Side 3 = ')
+        lbl_tri_s3.grid(row=2, column=0)
+        self.tri_s3 = tk.StringVar()
+        entry_tri_s3 = ttk.Entry(self.triangleSSS, width=10, textvariable=self.tri_s3)
+        entry_tri_s3.grid(row=2, column=1)
+
+        button = ttk.Button(self.triangleSSS, text='Calculate', command=self.calc_tri_SSS)
+        button.grid(row=3, column=0, columnspan=4, sticky='NSEW', padx=20, pady=20)
+
+        lbl_tri_SSS_area = ttk.Label(self.triangleSSS, text='area = ')
+        lbl_tri_SSS_area.grid(row=0, column=2)
+        self.tri_SSS_area = tk.StringVar()
+        entry_tri_SSS_area = ttk.Entry(self.triangleSSS, width=10, textvariable=self.tri_SSS_area, state='disable')
+        entry_tri_SSS_area.grid(row=0, column=3)
+
+        lbl_tri_SSS_peri = ttk.Label(self.triangleSSS, text='perimeter = ')
+        lbl_tri_SSS_peri.grid(row=1, column=2)
+        self.tri_SSS_peri = tk.StringVar()
+        entry_tri_SSS_peri = ttk.Entry(self.triangleSSS, width=10, textvariable=self.tri_SSS_peri, state='disable')
+        entry_tri_SSS_peri.grid(row=1, column=3)
+
+    def calc_tri_SSS(self):
+        self.tri_SSS_area.set(Geo_2D.Triangle_SSS(side_1=float(self.tri_s1.get()), side_2=float(self.tri_s2.get()), side_3=float(self.tri_s3.get())).area())
+        self.tri_SSS_peri.set(Geo_2D.Triangle_SSS(side_1=float(self.tri_s1.get()), side_2=float(self.tri_s2.get()), side_3=float(self.tri_s3.get())).perimeter())
 
     def sq(self):
         lbl_sq_base = ttk.Label(self.square, text='base = ')
